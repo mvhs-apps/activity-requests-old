ce4e4dc4b482db3504a4df5bbc59e5bc4f0e9547
arjunpat <ajpat1234@gmail.com>
2018-03-19 12:55:42 -0700

    a

--- public/js/new.js
+++ public/js/new.js
@@ -3,7 +3,7 @@
 var mainFormPart = document.getElementById('main-form-part');
 var fundrasierFormPart = document.getElementById('fundraiser-form-part');
 var donationFormPart = document.getElementById('donation-form-part');
-
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 var typeOfForm;
 
 var fundOrDonationShow = function(value) {
@@ -19,4 +19,65 @@ var fundOrDonationShow = function(value) {
 		fundrasierFormPart.style.display = 'none';
 		donationFormPart.style.display = 'none';
 	}
+}
+
+
+/*
+ * below is where
+ * all of the form validation
+ * functions are
+ * feel free to add more
+ * I've made them so most of them are reusable
+ */
+
+
+
+var capitalizeEachWordInStr = function(str) {
+	return str.toLowerCase().split(' ').map(function(word) {
+			if (word[0]) return word[0].toUpperCase() + word.substr(1);
+		}).join(' ');
+}
+
+var areAllArrayValuesLongerThan = function(arr, amount) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].length <= amount) {
+			return false;
+		}
+	}
+	return true;
+}
+
+var validateFullName = function(element) {
+	var value = element.value;
+	var parts = value.split(' ');
+
+	if (parts.length > 1 && areAllArrayValuesLongerThan(parts, 1)) {
+		// capitalizes each word
+		element.value = capitalizeEachWordInStr(element.value);
+		element.style.borderColor = '';
+		element.parentNode.querySelector('.feedback').style.display = 'none';
+	} else {
+		// changes border color and shows the feedback <span>
+		element.style.borderColor = '#d50000';
+		element.parentNode.querySelector('.feedback').style.display = 'block';
+	}
+}
+
+var validateEmail = function(element, shouldEndWith) {
+	// trims any extra white space off the value
+	element.value = element.value.trim();
+	var value = element.value;
+	// if no argument for that, sets defualt arg
+	if (!shouldEndWith) var shouldEndWith = '';
+
+	// tests for vailidity using the emailRegEx const defined above
+	if (emailRegEx.test(value) && value.endsWith(shouldEndWith)) {
+		element.style.borderColor = '';
+		element.parentNode.querySelector('.feedback').style.display = 'none';
+	} else {
+		// changes border color and shows the feedback <span>
+		element.style.borderColor = '#d50000';
+		element.parentNode.querySelector('.feedback').style.display = 'block';
+	}
+
 }
