588b5ad6dbf159e74700f9996fdd5ba440c34c36
Marc Bacvanski <marc.bacvanski@gmail.com>
2018-03-21 08:40:23 -0700

    email setup

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 global.__base = __dirname + '/';
 const PORT = 8000;
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,7 +30,7 @@ app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,43 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/new', function(req, res, next) {
-	res.render('new');
+router.get('/new', (req, res) => {
+  res.render('new');
 });
+
+let sendEmail = (email, type, id) => {
+  if (email && type && id) {
+    let emailValidator = require('email-validator');
+
+    let trackingLink = 'localhost:8000/track?id=' + id;
+    let message = '';
+    let subject = '';
+    if (type === 'confirmation') {
+      message = 'Hello!\n\nThank you for submitting a fundraising form! Your tracking' +
+          'code is ' + id + '. Follow this link ' + trackingLink + ' to track the status' +
+          'of your application.';
+    }
+    let send = require('gmail-send')({
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD,
+      to: email,
+      subject: subject,
+      text: message
+    });
+
+    // Override any default option and send email
+    send({}, function(err, response) {
+      if (!err) {
+        res.status(200).send({response: response});
+      } else {
+        res.status(400).send({error: err});
+      }
+    });
+  }
+};
 
 module.exports = router;
