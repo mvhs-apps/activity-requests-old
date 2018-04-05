29edbc05dc7bb0f34d918a1675fd11b47f95f106
Marc Bacvanski <marc.bacvanski@gmail.com>
2018-04-04 19:07:43 -0700

    Emailer works

---
+++ mailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
+
+module.exports.sendMail = (data) => {
+  const recipient = data.recipient;
+  const subject = data.subject;
+  const messageHTML = data.messageHTML;
+
+  return new Promise((resolve, reject) => {
+    if (!(recipient && subject && messageHTML)) {
+      reject('Not enough parameters');
+    }
+
+    const mailOptions = {
+      from: '"MVHS Approvals"' + process.env.GMAIL_USERNAME,
+      to: recipient,
+      subject: subject,
+      text: messageHTML.replace(/<(?:.|\n)*?>/gm, ''),
+      html: messageHTML,
+    };
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+
+    transporter.sendMail(mailOptions).
+        then(() => resolve()).
+        catch(() => reject());
+  });
+};
--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-const mailer = require('gmail-send');
+const mailer = require('./mailer');
 
 router.get('/', (req, res) => {
   res.render('index');
@@ -9,7 +9,6 @@ router.get('/', (req, res) => {
 router.get('/form', (req, res) => {
   res.render('form');
 });
-
 
 router.get('/track', (req, res) => {
   res.render('track');
@@ -21,37 +20,16 @@ router.get('/trackForm', (req, res) => {
 
 router.post('/email', (req, res) => {
   console.log('sending');
-  var id = 'asdfasdf';
-  sendEmail({
-    to: 'ajpat1234@gmail.com',
+  let id = 'asdfasdf';
+  mailer.sendMail({
+    recipient: 'marc.bacvanski@gmail.com',
     subject: 'Hello sd!',
-    html: `Some  smessage goes here: ${id}`
+    messageHTML: `Some message goes here: ${id}`,
+  }).then(() => {
+    console.log('successfully sent');
+  }).catch((err) => {
+    console.error('error ' + err);
   });
 });
-
-router.post('/')
-
-let sendEmail = (params) => {
-  if (params.to && params.subject && params.html) {
-    let send = mailer({
-      user: process.env.GMAIL_USERNAME,
-      pass: process.env.GMAIL_PASSWORD,
-      from: 'MVHS ASB Approvals',
-      to: params.to,
-      subject: params.subject,
-      html: params.html,
-    });
-
-    // Override any default option and send email
-    send({}, function(err, response) {
-      if (!err) {
-        // res.status(200).send({response: response});
-      } else {
-        console.error('error ' + err);
-        // res.status(400).send({error: err});
-      }
-    });
-  }
-};
 
 module.exports = router;
