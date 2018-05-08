5c21f52c970c08dfb362f6927f0787f931a0ff78
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-05-08 12:28:29 -0700

    TRACKING WORKING!!!!!

--- public/js/track.js
+++ public/js/track.js
@@ -1,5 +1,44 @@
 var trackingId = document.getElementById('trackingId');
 
-var trackForm = function() {
-	window.location.href = "trackForm/" + trackingId.value;
+window.onload = function() {
+	$("#trackingStatus").hide();
 }
+
+var trackForm = function() {
+	$("#inputId").hide();
+	$("#trackingStatus").show();
+
+	return firebase.database().ref('/test/').once('value').then(function(snapshot) {
+		array = snapshot;
+	  var totalHTML = "<tr>";
+	  if((array.child($("#trackingId").val()).child("approvedClubAdvisor").val() != undefined) && (array.child($("#trackingId").val()).child("approvedClubAdvisor").val() != null)) {
+	  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedClubAdvisor").val() + "</td>";
+	  } else {
+	  	totalHTML += "<td>pending...</td>";
+	  }
+	  if((array.child($("#trackingId").val()).child("library").val() != undefined) && (array.child($("#trackingId").val()).child("library").val() != null)) {
+	  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("library").val() + "</td>";
+	  } else {
+	  	totalHTML += "<td>pending...</td>";
+	  }
+	  if((array.child($("#trackingId").val()).child("other").val() != undefined) && (array.child($("#trackingId").val()).child("other").val() != null)) {
+	  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("other").val() + "</td>";
+	  } else {
+	  	totalHTML += "<td>pending...</td>";
+	  }
+	  totalHTML += "</tr>";
+	  $("#table").append(totalHTML);
+	});
+}
+
+function snapshotToArray(snapshot) {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+      let item = childSnapshot.val(); 
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+  });
+
+  return returnArr;
+};
