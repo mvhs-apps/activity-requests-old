a887cd98065df944645110884bdcd7e8f0a7224c
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-05-09 11:13:59 -0700

    tracking page updated with dynamic table based on which approvals are neeeded

--- public/js/track.js
+++ public/js/track.js
@@ -10,22 +10,57 @@ var trackForm = function() {
 
 	return firebase.database().ref('/test/').once('value').then(function(snapshot) {
 		array = snapshot;
-	  var totalHTML = "<tr>";
-	  if((array.child($("#trackingId").val()).child("approvedClubAdvisor").val() != undefined) && (array.child($("#trackingId").val()).child("approvedClubAdvisor").val() != null)) {
-	  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedClubAdvisor").val() + "</td>";
-	  } else {
-	  	totalHTML += "<td>pending...</td>";
-	  }
-	  if((array.child($("#trackingId").val()).child("library").val() != undefined) && (array.child($("#trackingId").val()).child("library").val() != null)) {
-	  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("library").val() + "</td>";
-	  } else {
-	  	totalHTML += "<td>pending...</td>";
-	  }
-	  if((array.child($("#trackingId").val()).child("other").val() != undefined) && (array.child($("#trackingId").val()).child("other").val() != null)) {
-	  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("other").val() + "</td>";
-	  } else {
-	  	totalHTML += "<td>pending...</td>";
-	  }
+
+		var totalHTML = "<tr>";
+
+		if((array.child($("#trackingId").val()).child("approvedClubAdvisor").val() != undefined) && (array.child($("#trackingId").val()).child("approvedClubAdvisor").val() != null)) {
+		  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedClubAdvisor").val() + "</td>";
+		  } else {
+		  	totalHTML += "<td>pending...</td>";
+		  }
+
+
+		if(array.child($("#trackingId").val()).child("campus").child("cafeteria").val() === true) {
+			$("#tableHeaders").append("<th> Cafeteria </th>");
+			if((array.child($("#trackingId").val()).child("approvedCafeteria").val() != undefined) && (array.child($("#trackingId").val()).child("approvedCafeteria").val() != null)) {
+			  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedCafeteria").val() + "</td>";
+			} else {
+			 	totalHTML += "<td>pending...</td>";
+			}
+		}
+		if(array.child($("#trackingId").val()).child("campus").child("gym").val() === true) {
+			$("#tableHeaders").append("<th> Gym </th>");
+			if((array.child($("#trackingId").val()).child("approvedGym").val() != undefined) && (array.child($("#trackingId").val()).child("approvedGym").val() != null)) {
+			  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedGym").val() + "</td>";
+			} else {
+			 	totalHTML += "<td>pending...</td>";
+			}
+		}
+		if(array.child($("#trackingId").val()).child("campus").child("library").val() === true) {
+			$("#tableHeaders").append("<th> Library </th>");
+			if((array.child($("#trackingId").val()).child("approvedLibrary").val() != undefined) && (array.child($("#trackingId").val()).child("approvedLibrary").val() != null)) {
+			  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedLibrary").val() + "</td>";
+			} else {
+			 	totalHTML += "<td>pending...</td>";
+			}
+		}
+		if(array.child($("#trackingId").val()).child("campus").child("ccc").val() === true) {
+			$("#tableHeaders").append("<th> College and Career Center </th>");
+			if((array.child($("#trackingId").val()).child("approvedCCC").val() != undefined) && (array.child($("#trackingId").val()).child("approvedCCC").val() != null)) {
+			  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedCCC").val() + "</td>";
+			} else {
+			 	totalHTML += "<td>pending...</td>";
+			}
+		}
+
+
+		$("#tableHeaders").append("<th> ASB </th>");
+		if((array.child($("#trackingId").val()).child("approvedASB").val() != undefined) && (array.child($("#trackingId").val()).child("approvedASB").val() != null)) {
+		  	totalHTML += "<td>" + array.child($("#trackingId").val()).child("approvedASB").val() + "</td>";
+		} else {
+		 	totalHTML += "<td>pending...</td>";
+		}
+
 	  totalHTML += "</tr>";
 	  $("#table").append(totalHTML);
 	});
