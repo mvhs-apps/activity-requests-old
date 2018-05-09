56366524e8bf21033c05c3e4672731d6b6a8810b
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-05-09 11:54:22 -0700

    all emails and tracking working on local server

--- public/js/approve.js
+++ public/js/approve.js
@@ -1,7 +1,27 @@
 window.onload = function() {
 	var link = String(window.location.href);
 	if(link.indexOf("?") != -1) {
-		var id = link.substr(link.indexOf("?") + 1, link.length);
-		firebase.database().ref('test/' + id + "/approvedClubAdvisor").set("yes");
+		var endOfLink = link.substr(link.indexOf("?") + 1, link.length);
+		var nameOfApproval = endOfLink.substr(endOfLink.indexOf("?") + 1, endOfLink.length);
+		var id = endOfLink.substr(0, endOfLink.indexOf("?"));
+
+		if(nameOfApproval === "clubAdvisor") {
+			firebase.database().ref('test/' + id + "/approvedClubAdvisor").set("approved");
+		}
+		else if(nameOfApproval === "cafeteria") {
+			firebase.database().ref('test/' + id + "/approvedCafeteria").set("approved");
+		}
+		else if(nameOfApproval === "gym") {
+			firebase.database().ref('test/' + id + "/approvedGym").set("approved");
+		}
+		else if(nameOfApproval === "library") {
+			firebase.database().ref('test/' + id + "/approvedLibrary").set("approved");
+		}
+		else if(nameOfApproval === "ccc") {
+			firebase.database().ref('test/' + id + "/approvedCCC").set("approved");
+		}
+		else if(nameOfApproval === "ASB") {
+			firebase.database().ref('test/' + id + "/approvedASB").set("approved");
+		}
 	}	
 }
--- router.js
+++ router.js
@@ -55,42 +55,42 @@ router.post('/submit_form', (req, res) => {
   mailer.sendMail({
     recipient: formObject.general.student_email,
     subject: "Confirmation for submitting your approval",
-    messageHTML: "You have submitted an approval, you can track it's progress here: <a href='https://mvhs-approvals.herokuapp.com/track'>https://mvhs-approvals.herokuapp.com/track</a> using the id: '" + formObject.id_num + "'"
+    messageHTML: "You have submitted an approval, you can track it's progress here: <a href='localhost:8080/track'>localhost:8080/track</a> using the id: '" + formObject.id_num + "'"
   }).then((data) => {
     return mailer.sendMail({
       recipient: formObject.general.advisor_email,
       subject: "A student is requesting your approval",
-      messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='https://mvhs-approvals.herokuapp.com/approve?" + formObject.id_num + "'>Accept</a></button><button><a href='https://mvhs-approvals.herokuapp.com/reject?" + formObject.id_num + "'>Reject</a></button>"});
+      messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?clubAdvisor'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?clubAdvisor'>Reject</a></button>"});
   }).then((data) => {
     if(formObject.campus.cafeteria === true) {
       return mailer.sendMail({
         recipient: "paran.sonthalia@gmail.com",
-        subject: "cafeteria test",
-        messageHTML: "test"});
+        subject: "A student is requesting your approval for use of the Cafeteria",
+        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?cafeteria'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?cafeteria'>Reject</a></button>"});
     }
     return;
   }).then((data) => {
     if(formObject.campus.gym === true) {
       return mailer.sendMail({
         recipient: "paran.sonthalia@gmail.com",
-        subject: "gym test",
-        messageHTML: "test"});
+        subject: "A student is requesting your approval for use of the Gym",
+        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?gym'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?gym'>Reject</a></button>"});
     }
     return;
   }).then((data) => {
     if(formObject.campus.library === true) {
       return mailer.sendMail({
         recipient: "paran.sonthalia@gmail.com",
-        subject: "library test",
-        messageHTML: "test"});
+        subject: "A student is requesting your approval for use of the library",
+        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?library'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?library'>Reject</a></button>"});
     }
     return;
   }).then((data) => {
     if(formObject.campus.ccc === true) {
       return mailer.sendMail({
         recipient: "paran.sonthalia@gmail.com",
-        subject: "ccc test",
-        messageHTML: "test"});
+        subject: "A student is requesting your approval for use of the College and Career Center",
+        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?ccc'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?ccc'>Reject</a></button>"});
     }
     return;
   }).then((data) => {
