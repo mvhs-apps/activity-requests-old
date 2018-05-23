69443c7b308ee98afac53858af9ede845b46ab86
Marc Bacvanski <marc.bacvanski@gmail.com>
2018-05-22 17:39:34 -0700

    Update emailing

--- router.js
+++ router.js
@@ -38,7 +38,7 @@ router.post('/email', (req, res) => {
   mailer.sendMail({
     recipient: req.body.recipient,
     subject: req.body.subject,
-    messageHTML: req.body.message
+    messageHTML: req.body.message,
   }).then(() => {
     console.log('successfully sent');
   }).catch((err) => {
@@ -48,59 +48,90 @@ router.post('/email', (req, res) => {
 
 router.post('/submit_form', (req, res) => {
 
-  var formObject = JSON.parse(req.body.form_data);
+  let formObject = JSON.parse(req.body.form_data);
 
-  console.log(formObject);
-
-  mailer.sendMail({
+  let studentEmail = mailer.sendMail({
     recipient: formObject.general.student_email,
-    subject: "Confirmation for submitting your approval",
-    messageHTML: "You have submitted an approval, you can track it's progress here: <a href='localhost:8080/track'>localhost:8080/track</a> using the id: '" + formObject.id_num + "'"
-  }).then((data) => {
-    return mailer.sendMail({
-      recipient: formObject.general.advisor_email,
-      subject: "A student is requesting your approval",
-      messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?clubAdvisor'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?clubAdvisor'>Reject</a></button>"});
-  }).then((data) => {
-    if(formObject.campus.cafeteria === true) {
-      return mailer.sendMail({
-        recipient: "paran.sonthalia@gmail.com",
-        subject: "A student is requesting your approval for use of the Cafeteria",
-        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?cafeteria'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?cafeteria'>Reject</a></button>"});
-    }
-    return;
-  }).then((data) => {
-    if(formObject.campus.gym === true) {
-      return mailer.sendMail({
-        recipient: "paran.sonthalia@gmail.com",
-        subject: "A student is requesting your approval for use of the Gym",
-        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?gym'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?gym'>Reject</a></button>"});
-    }
-    return;
-  }).then((data) => {
-    if(formObject.campus.library === true) {
-      return mailer.sendMail({
-        recipient: "paran.sonthalia@gmail.com",
-        subject: "A student is requesting your approval for use of the library",
-        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?library'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?library'>Reject</a></button>"});
-    }
-    return;
-  }).then((data) => {
-    if(formObject.campus.ccc === true) {
-      return mailer.sendMail({
-        recipient: "paran.sonthalia@gmail.com",
-        subject: "A student is requesting your approval for use of the College and Career Center",
-        messageHTML: formObject.general.student_name + " has submitted an approval for " + formObject.general.club_name + ". Please accept or reject the approval below.<br /><button><a href='localhost:8080/approve?" + formObject.id_num + "?ccc'>Accept</a></button><button><a href='localhost:8080/reject?" + formObject.id_num + "?ccc'>Reject</a></button>"});
-    }
-    return;
-  }).then((data) => {
+    subject: 'Confirmation for submitting your approval',
+    messageHTML: 'You have submitted an approval, you can track it\'s progress here: <a href=\'localhost:8080/track\'>localhost:8080/track</a> using the id: \'' +
+    formObject.id_num + '\'',
+  });
+  let advisorEmail = mailer.sendMail({
+    recipient: formObject.general.advisor_email,
+    subject: 'A student is requesting your approval',
+    messageHTML: formObject.general.student_name +
+    ' has submitted an approval for ' + formObject.general.club_name +
+    '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+    formObject.id_num +
+    '?clubAdvisor\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+    formObject.id_num + '?clubAdvisor\'>Reject</a></button>',
+  });
+  let cafeterialEmail = (formObject.campus.cafeteria === true)
+      ? mailer.sendMail({
+        recipient: 'paran.sonthalia@gmail.com',
+        subject: 'A student is requesting your approval for use of the Cafeteria',
+        messageHTML: formObject.general.student_name +
+        ' has submitted an approval for ' + formObject.general.club_name +
+        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        formObject.id_num +
+        '?cafeteria\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        formObject.id_num + '?cafeteria\'>Reject</a></button>',
+      })
+      : Promise.resolve();
+  let gymEmail = (formObject.campus.gym === true)
+      ? mailer.sendMail({
+        recipient: 'paran.sonthalia@gmail.com',
+        subject: 'A student is requesting your approval for use of the Gym',
+        messageHTML: formObject.general.student_name +
+        ' has submitted an approval for ' + formObject.general.club_name +
+        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        formObject.id_num +
+        '?gym\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        formObject.id_num + '?gym\'>Reject</a></button>',
+      })
+      : Promise.resolve();
+  let libraryEmail = (formObject.campus.library === true)
+      ? mailer.sendMail({
+        recipient: 'paran.sonthalia@gmail.com',
+        subject: 'A student is requesting your approval for use of the library',
+        messageHTML: formObject.general.student_name +
+        ' has submitted an approval for ' + formObject.general.club_name +
+        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        formObject.id_num +
+        '?library\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        formObject.id_num + '?library\'>Reject</a></button>',
+      })
+      : Promise.resolve();
+  let cccEmail = (formObject.campus.ccc === true)
+      ? mailer.sendMail({
+        recipient: 'paran.sonthalia@gmail.com',
+        subject: 'A student is requesting your approval for use of the College and Career Center',
+        messageHTML: formObject.general.student_name +
+        ' has submitted an approval for ' + formObject.general.club_name +
+        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        formObject.id_num +
+        '?ccc\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        formObject.id_num + '?ccc\'>Reject</a></button>',
+      })
+      : Promise.resolve();
+
+  let mailTasks = [
+    studentEmail,
+    advisorEmail,
+    cafeterialEmail,
+    gymEmail,
+    libraryEmail,
+    cccEmail];
+
+  Promise.all(mailTasks).then(() => {
     res.json({
-      success: true
+      success: true,
     });
   }).catch((err) => {
-    console.error('error sending mail ' + err);  
+    console.error('error sending mail ' + err);
   });
-});
+})
+;
 
 router.post('/');
 
