a859122a7ee25a51da8d1634189812d2f8afd8ec
Paran Sonthalia <paran.sonthalia@gmail.com>
2018-05-22 21:08:50 -0700

    converted localhost to heroku domain

--- router.js
+++ router.js
@@ -57,18 +57,18 @@ router.post('/submit_form', (req, res) => {
   let studentEmail = mailer.sendMail({
     recipient: formObject.general.student_email,
     subject: 'Confirmation for submitting your approval',
-    messageHTML: 'You have submitted an approval, you can track it\'s progress here: <a href=\'localhost:8080/track\'>localhost:8080/track</a> using the id: \'' +
-    formObject.id_num + '\'',
+    messageHTML: '<div style="width: 900px; background-color: gray">You have submitted an approval, you can track it\'s progress here: <a href=\'https://mvhs-approvals.herokuapp.com/track\'>https://mvhs-approvals.herokuapp.com/track</a> using the id: \'' +
+    formObject.id_num + '\'' + '</div>',
   });
   let advisorEmail = mailer.sendMail({
     recipient: formObject.general.advisor_email,
     subject: 'A student is requesting your approval',
-    messageHTML: formObject.general.student_name +
+    messageHTML: '<div style="width: 900px; background-color: gray">' + formObject.general.student_name +
     ' has submitted an approval for ' + formObject.general.club_name +
-    '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+    '. Please accept or reject the approval below.<br /><button style="background-color: green"><a href=\'https://mvhs-approvals.herokuapp.com/approve?' +
     formObject.id_num +
-    '?clubAdvisor\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
-    formObject.id_num + '?clubAdvisor\'>Reject</a></button>',
+    '?clubAdvisor\'>Accept</a></button><button><a href=\'https://mvhs-approvals.herokuapp.com/reject?' +
+    formObject.id_num + '?clubAdvisor\'>Reject</a></button>' + '</div>',
   });
   let cafeterialEmail = (formObject.campus.cafeteria === true)
       ? mailer.sendMail({
@@ -76,9 +76,9 @@ router.post('/submit_form', (req, res) => {
         subject: 'A student is requesting your approval for use of the Cafeteria',
         messageHTML: formObject.general.student_name +
         ' has submitted an approval for ' + formObject.general.club_name +
-        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        '. Please accept or reject the approval below.<br /><button><a href=\'https://mvhs-approvals.herokuapp.com/approve?' +
         formObject.id_num +
-        '?cafeteria\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        '?cafeteria\'>Accept</a></button><button><a href=\'https://mvhs-approvals.herokuapp.com/reject?' +
         formObject.id_num + '?cafeteria\'>Reject</a></button>',
       })
       : Promise.resolve();
@@ -88,9 +88,9 @@ router.post('/submit_form', (req, res) => {
         subject: 'A student is requesting your approval for use of the Gym',
         messageHTML: formObject.general.student_name +
         ' has submitted an approval for ' + formObject.general.club_name +
-        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        '. Please accept or reject the approval below.<br /><button><a href=\'https://mvhs-approvals.herokuapp.com/approve?' +
         formObject.id_num +
-        '?gym\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        '?gym\'>Accept</a></button><button><a href=\'https://mvhs-approvals.herokuapp.com/reject?' +
         formObject.id_num + '?gym\'>Reject</a></button>',
       })
       : Promise.resolve();
@@ -100,9 +100,9 @@ router.post('/submit_form', (req, res) => {
         subject: 'A student is requesting your approval for use of the library',
         messageHTML: formObject.general.student_name +
         ' has submitted an approval for ' + formObject.general.club_name +
-        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        '. Please accept or reject the approval below.<br /><button><a href=\'https://mvhs-approvals.herokuapp.com/approve?' +
         formObject.id_num +
-        '?library\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        '?library\'>Accept</a></button><button><a href=\'https://mvhs-approvals.herokuapp.com/reject?' +
         formObject.id_num + '?library\'>Reject</a></button>',
       })
       : Promise.resolve();
@@ -112,9 +112,9 @@ router.post('/submit_form', (req, res) => {
         subject: 'A student is requesting your approval for use of the College and Career Center',
         messageHTML: formObject.general.student_name +
         ' has submitted an approval for ' + formObject.general.club_name +
-        '. Please accept or reject the approval below.<br /><button><a href=\'localhost:8080/approve?' +
+        '. Please accept or reject the approval below.<br /><button><a href=\'https://mvhs-approvals.herokuapp.com/approve?' +
         formObject.id_num +
-        '?ccc\'>Accept</a></button><button><a href=\'localhost:8080/reject?' +
+        '?ccc\'>Accept</a></button><button><a href=\'https://mvhs-approvals.herokuapp.com/reject?' +
         formObject.id_num + '?ccc\'>Reject</a></button>',
       })
       : Promise.resolve();
